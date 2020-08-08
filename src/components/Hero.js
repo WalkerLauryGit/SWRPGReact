d6b3044470ff598a32beb84f73af214253aabfc8
Walker Laury <walkerlaury@walkers-mbp.lan>
2020-08-08 18:31:06 -0500

    Got started with individual components for the list. Router is working

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Characters() {
+    return (
+        <div className="info row no-padding no-margin">
+            <div className="img-container col s12 m12 l6 xl6 no-padding">
+                <img src="" className="responsive-img" />
+            </div>
+            <div className="col s12 m12 l6 xl6 no-padding">
+                <div className="name">Anakin Skywalker</div>
+                <div className="description">
+                <p>Skywalker was born in the year 41 BBY before moving to the Outer Rim planet Tatooine during the Age of the Republic. Although he had a mother, the slave Shmi Skywalker, the truth about his parentage was that Anakin had no father—leading Jedi Master Qui-Gon Jinn to suspect that Skywalker's conception was the work of the midi-chlorians. Recognizing the boy's unprecedented connection to the Force, Jinn was determined to see Skywalker trained as a member of the Jedi Order, the task of which ultimately fell to his Padawan Obi-Wan Kenobi following Jinn's death by Darth Maul in 32 BBY.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
